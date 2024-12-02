6aea6c1b367503c17d2213a2471a31ae7e73f911
Enzo Cortez <enzo.cortez@udla.edu.ec>
2024-12-01 23:57:22 -0500

    Modificacion de About

--- Notes/Models/About.cs
+++ Notes/Models/About.cs
@@ -3,7 +3,7 @@
 internal class About
 {
     public string Title => AppInfo.Name;
-    public string Version => AppInfo.VersionString;
+    //public string Version => AppInfo.VersionString;
     public string MoreInfoUrl => "https://aka.ms/maui";
-    public string Message => "This app is written in XAML and C# with .NET MAUI.";
+    public string Message => "Mucho gusto mi nombre es Enzo y soy estudiante de Ing. en software";
 }
